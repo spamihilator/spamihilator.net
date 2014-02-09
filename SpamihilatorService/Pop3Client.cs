69ccb51c244e7fd4386d517c2c77e9c28496e74f
Michel Kraemer <michel@spamihilator.com>
2014-02-09 16:50:50 +0100

    Added more methods to POP3 client

--- SpamihilatorService/Pop3Client.cs
+++ SpamihilatorService/Pop3Client.cs
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using log4net;
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Spamihilator {
   /// <summary>
@@ -24,8 +27,53 @@ namespace Spamihilator {
     /// </summary>
     /// <param name="success">true if the asynchronous operation was
     /// successful</param>
-    /// <param name="msg">the operation's result message</param>
-    public delegate void ClientCallback(bool success, String msg);
+    /// <param name="status">the operation's result message</param>
+    public delegate void ClientCallback(bool success, String status);
+
+    /// <summary>
+    /// A callback that will be invoked when a list of unique IDs
+    /// has been received from the server
+    /// </summary>
+    /// <param name="success">true if the list of unique IDs was
+    /// received successfully</param>
+    /// <param name="status">the operation's result message (an
+    /// error message in case <code>success</code> is false)</param>
+    /// <param name="uniqueIds">a dictionary mapping message IDs to unique
+    /// IDs (empty in case <code>success</code> is false)</param>
+    public delegate void ListUniqueIdsCallback(bool success, String status,
+      Dictionary<long, String> uniqueIds);
+
+    /// <summary>
+    /// A callback that will be invoked when a list of messages
+    /// has been received from the server
+    /// </summary>
+    /// <param name="success">true if the list of messages was
+    /// received successfully</param>
+    /// <param name="status">the operation's result message (an
+    /// error message in case <code>success</code> is false)</param>
+    /// <param name="messages">a dictionary mapping message IDs to message
+    /// contents (empty in case <code>success</code> is false)</param>
+    public delegate void ListCallback(bool success, String status,
+      Dictionary<long, long> messages);
+
+    /// <summary>
+    /// A callback that will be invoked when a message has been
+    /// retrieved from the server
+    /// </summary>
+    /// <param name="success">true if the message was received
+    /// successfully</param>
+    /// <param name="status">the operation's result message (an
+    /// error message in case <code>success</code> is false)</param>
+    /// <param name="message">the message contents (empty in case
+    /// <code>success</code> is false)</param>
+    public delegate void RetrieveCallback(bool success, String status,
+      String message);
+
+    /// <summary>
+    /// Logs communication with the peer to a file
+    /// </summary>
+    private static readonly ILog log = LogManager.GetLogger(
+      System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
     /// <summary>
     /// Checks if the given message is an error message
@@ -116,6 +164,116 @@ namespace Spamihilator {
     /// message has been sent and the response has been received</param>
     public void Noop(ClientCallback callback) {
       SendAndReceive("NOOP", callback);
+    }
+
+    /// <summary>
+    /// Deletes a message from the server
+    /// </summary>
+    /// <param name="id">the message's ID</param>
+    /// <param name="callback">a method that will be called after the
+    /// message has been deleted and the response has been received</param>
+    public void Delete(long id, ClientCallback callback) {
+      SendAndReceive("DELE " + id, callback);
+    }
+
+    /// <summary>
+    /// Lists unique IDs of messages
+    /// </summary>
+    /// <param name="callback">a method that will be called after
+    /// the list of unique IDs has been received from the server</param>
+    public void ListUniqueIds(ListUniqueIdsCallback callback) {
+      ListInternal<long, String>("UIDL", long.Parse, s => s,
+        (s, m, d) => callback(s, m, d), "unique-id");
+    }
+
+    /// <summary>
+    /// Lists messages
+    /// </summary>
+    /// <param name="callback">a method that will be called after
+    /// the list of messages has been received from the server</param>
+    public void List(ListCallback callback) {
+      ListInternal<long, long>("LIST", long.Parse, long.Parse,
+        (s, m, d) => callback(s, m, d), "scan");
+    }
+
+    /// <summary>
+    /// Send a list command to the server and reads its multi-line response
+    /// consisting of key-value listings.
+    /// </summary>
+    /// <typeparam name="K">the type of keys to read</typeparam>
+    /// <typeparam name="V">the type of values to read</typeparam>
+    /// <param name="command">the command to send to the server</param>
+    /// <param name="mk">a function that parses a string to a key</param>
+    /// <param name="mv">a function that parses a string to a value</param>
+    /// <param name="callback">a callback to call after the whole
+    /// multi-line response has been read and after all key-value pairs
+    /// have been converted.</param>
+    /// <param name="listingName">a listing's name (used to produce a
+    /// human-readable error message if a listing is invalid)</param>
+    private void ListInternal<K, V>(String command, Func<String, K> mk,
+        Func<String, V> mv, Action<bool, String, Dictionary<K, V>> callback,
+        String listingName) {
+      SendLine(command, () => {
+        Receive(ur => {
+          var listings = new Dictionary<K, V>();
+          if (!IsOK(ur)) {
+            callback(false, ur, listings);
+          } else {
+            ReceiveCallback handleLine = null;
+            handleLine = line => {
+              if (line == ".") {
+                callback(true, ur, listings);
+              } else {
+                int sp = line.IndexOf(' ');
+                if (sp < 0) {
+                  sp = line.IndexOf('\t');
+                }
+                if (sp <= 0) {
+                  log.Error("POP3 server returned invalid " + listingName +
+                    " listing");
+                } else {
+                  K key = mk(line.Substring(0, sp));
+                  V value = mv(line.Substring(sp + 1));
+                  listings.Add(key, value);
+                }
+                Receive(handleLine);
+              }
+            };
+            Receive(handleLine);
+          }
+        });
+      });
+    }
+
+    /// <summary>
+    /// Retrieve a message from the server
+    /// </summary>
+    /// <param name="id">the message's ID</param>
+    /// <param name="callback">a callback that will be invoked after
+    /// the message was received successfully</param>
+    public void Retrieve(long id, RetrieveCallback callback) {
+      SendLine("RETR " + id, () => {
+        Receive(ur => {
+          var message = new StringBuilder();
+          if (!IsOK(ur)) {
+            callback(false, ur, message.ToString());
+          } else {
+            ReceiveCallback handleLine = null;
+            handleLine = line => {
+              if (line == ".") {
+                callback(true, ur, message.ToString());
+              } else {
+                if (line.StartsWith(".")) {
+                  line = line.Substring(1);
+                }
+                message.AppendLine(line);
+                Receive(handleLine);
+              }
+            };
+            Receive(handleLine);
+          }
+        });
+      });
     }
   }
 }
